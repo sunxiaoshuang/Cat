9a01c9547f8a11f6acf5d38f18d37adfe6cb8c76
xainideyun <sunxsaljj@163.com>
2018-05-17 11:37:47 +0800

    【孙小双】小程序代码

--- JdCat.Cat.Weixin/client/pages/index/index.js
+++ JdCat.Cat.Weixin/client/pages/index/index.js
@@ -22,7 +22,7 @@ Page({
         qcloud.login({
             success(result) {
                 if (result) {
-                    util.showSuccess('登录成功')
+                    util.showSuccess('登录成功');
                     that.setData({
                         userInfo: result,
                         logged: true
@@ -53,6 +53,80 @@ Page({
                 console.log('登录失败', error)
             }
         })
+    },
+
+    bindGetUserInfo: function (e) {
+      if (this.data.logged) return;
+
+      util.showBusy('正在登录');
+
+      var that = this;
+      var userInfo = e.detail.userInfo;
+
+      // 查看是否授权
+      wx.getSetting({
+        success: function (res) {
+          if (res.authSetting['scope.userInfo']) {
+
+            // 检查登录是否过期
+            wx.checkSession({
+              success: function () {
+                // 登录态未过期
+                util.showSuccess('登录成功');
+                that.setData({
+                  userInfo: userInfo,
+                  logged: true
+                })
+              },
+
+              fail: function () {
+                qcloud.clearSession();
+                // 登录态已过期，需重新登录
+                var options = {
+                  encryptedData: e.detail.encryptedData,
+                  iv: e.detail.iv,
+                  userInfo: userInfo
+                }
+                that.doLogin(options);
+              },
+            });
+          } else {
+            util.showModel('用户未授权', e.detail.errMsg);
+          }
+        }
+      });
+    },
+
+    doLogin: function(options) {
+      var that = this;
+
+      wx.login({
+        success: function (loginResult) {
+          var loginParams = {
+            code: loginResult.code,
+            encryptedData: options.encryptedData,
+            iv: options.iv,
+          }
+          qcloud.requestLogin({
+            loginParams, success() {
+              util.showSuccess('登录成功');
+
+              that.setData({
+                userInfo: options.userInfo,
+                logged: true
+              })
+            },
+            fail(error) {
+              util.showModel('登录失败', error)
+              console.log('登录失败', error)
+            }
+          });
+        },
+        fail: function (loginError) {
+          util.showModel('登录失败', loginError)
+          console.log('登录失败', loginError)
+        },
+      });
     },
 
     // 切换是否带有登录态
--- JdCat.Cat.Weixin/server/config.js
+++ JdCat.Cat.Weixin/server/config.js
@@ -3,7 +3,7 @@ const CONF = {
     rootPathname: '',
 
     // 微信小程序 App ID
-    appId: 'wx7fc7dac038048c37',
+    appId: '',
 
     // 微信小程序 App Secret
     appSecret: '',
@@ -21,7 +21,7 @@ const CONF = {
         port: 3306,
         user: 'root',
         db: 'cAuth',
-        pass: 'wx7fc7dac038048c37',
+        pass: '',
         char: 'utf8mb4'
     },
 
